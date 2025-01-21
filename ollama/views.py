76703b9e7ef09f3e33f0fa6c070c67479c741b24
fracalrod3 <fracalrod3@alum.us.es>
2025-01-21 13:51:10 +0100

    feat: Ollama integrated

--- ollama/views.py
+++ ollama/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .ollama_service import get_ollama_response
+from .form import PromptForm
 
-# Create your views here.
+def ollama(request):
+    response = None
+    if request.method == "POST":
+        form = PromptForm(request.POST)
+        if form.is_valid():
+            prompt = form.cleaned_data['description']
+            response = get_ollama_response(prompt)
+    else:
+        form = PromptForm()
+
+    return render(request, 'ollama.html', {'form': form, 'response': response})
