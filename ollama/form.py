178d18fdb917190201a1b89bb432c97db9c15f88
fracalrod3 <fracalrod3@alum.us.es>
2025-01-21 18:47:40 +0100

    feat: Ollama Total Integration

--- ollama/form.py
+++ ollama/form.py
@@ -1,12 +1,10 @@
 from django import forms
+from .models import Prompt
 
-
-class PromptForm(forms.Form):
-    description = forms.CharField(
-        label="Write here your request",
-        max_length=255,
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Enter here your prompt'
-        })
-    )
+class PromptForm(forms.ModelForm):
+    class Meta:
+        model = Prompt
+        fields = ['request']  # Remove the 'ai' field from the form
+        widgets = {
+            'request': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
