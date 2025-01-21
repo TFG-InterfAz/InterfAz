abd88553671cac08cfcabaffe119c9a4105e4eb2
fracalrod3 <fracalrod3@alum.us.es>
2025-01-21 19:11:05 +0100

    feat: Unifying prompts

--- InterfAz/admin.py
+++ InterfAz/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from trial.models import Item
+from ollama.models import Prompt
 from starcoder.models import Prompt
 # Register your models here.
 admin.site.register(Item)
